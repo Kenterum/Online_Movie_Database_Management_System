04ca7d1b5a9be78cd1756ee3885661357359ee0b
arzum15988 <119140817+arzum15988@users.noreply.github.com>
2023-12-03 01:14:22 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie m1 = new Movie("Inception", "Christopher Nolan", 2010, 148);
+        System.out.println(m1);
+
+        Movie m2 = new Movie("Tutek sesi", "Rasim Ocagov", 1975, 82);
+        System.out.println(m2);
+
+        Movie m3 = new Movie("Ogey Ana", "Habib İsmailov", 1958, 81);
+        System.out.println(m3);
+
+    }
+}
