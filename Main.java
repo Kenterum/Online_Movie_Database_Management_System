b38e89229f77539554f7826275488e9ee3d51e83
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-12-03 21:07:51 +0400

    Update main class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MovieTest.adding( mBase1, Movie m1, Movie m2, Movie m3);
+        MovieTest.removing(null, null);
+        MovieTest.retrievedMovie(null, null);
+    }
+}
