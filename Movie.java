e5e30d9cbaa3497562aa4a6b3064bbcf77ba4a5a
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-12-08 10:21:08 +0400

    Movie class Enhanchemenst

--- Movie.java
+++ Movie.java
@@ -1,4 +1,6 @@
-public class Movie {
+import java.io.Serializable;
+
+public class Movie implements Serializable {
     private String title;
     private String director;
     private int releaseYear;
