2d659a36a4af139bbeedd57cd0760e5b628bd83a
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-12-08 10:16:41 +0400

    clear

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        MovieTest.adding( mBase1, Movie m1, Movie m2, Movie m3);
-        MovieTest.removing(null, null);
-        MovieTest.retrievedMovie(null, null);
+        MovieTest mBase = new MovieTest();
+
+        // Test Add
+        MovieTest.testAdd(mBase);
+
+        // Test Remove
+        MovieTest.testRemove(mBase);
+
+        // Test Retrieve
+        MovieTest.testRetrieve(mBase);
     }
 }
