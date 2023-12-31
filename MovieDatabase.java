b5667e0e2062591a0110a67005071d9a43980538
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-12-31 18:16:05 +0400

    Implement basic functionality for MovieDatabase class

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -1,5 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieDatabase {
     private List<Movie> movies;
@@ -7,11 +11,14 @@ public class MovieDatabase {
     public MovieDatabase() {
         this.movies = new ArrayList<>();
     }
+     private static final String CSV_FILE_PATH = "movies.csv";
 
     public void addMovie(Movie movie) {
         try {
             if (!movies.contains(movie)) {
                 movies.add(movie);
+                writeMovieToCSV(movie);
+
                 System.out.println("Movie is added to database: " + movie.getTitle());
             } else {
                 System.out.println("Movie is already existing in the database: " + movie.getTitle());
@@ -21,17 +28,34 @@ public class MovieDatabase {
         }
     }
 
+    private void writeMovieToCSV(Movie movie) {
+        try (FileWriter writer = new FileWriter("data/movies.csv", true)) {
+            writer.append(String.format("%s,%s,%d,%d%n",
+                    movie.getTitle(),
+                    movie.getDirector(),
+                    movie.getReleaseYear(),
+                    movie.getRunningTime()));
+        } catch (IOException e) {
+            System.out.println("Error writing to CSV file: " + e.getMessage());
+        }
+    }
+
+
+
     public void removeMovie(Movie movie) {
-        try {
+       
             if (movies.contains(movie)) {
-                movies.remove(movie);
+                try {
+                    movies.remove(movie);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+              
                 System.out.println("Movie is removed from the database: " + movie.getTitle());
             } else {
                 System.out.println("Movie is not found in the database: " + movie.getTitle());
             }
-        } catch (Exception e) {
-            System.out.println("An error occurred while removing the movie: " + e.getMessage());
-        }
+        
     }
 
     public Movie retrieveMovie(String title) {
@@ -45,5 +69,25 @@ public class MovieDatabase {
             System.out.println("An error occurred while retrieving the movie: " + e.getMessage());
         }
         return null;
+    }
+
+   
+
+    public List<Movie> getMoviesSortedByTitle() {
+        return movies.stream()
+                .sorted(Comparator.comparing(Movie::getTitle))
+                .collect(Collectors.toList());
+    }
+
+    public List<Movie> getMoviesReleasedAfterYear(int year) {
+        return movies.stream()
+                .filter(movie -> movie.getReleaseYear() > year)
+                .collect(Collectors.toList());
+    }
+
+    public int getTotalWatchTime() {
+        return movies.stream()
+                .mapToInt(Movie::getRunningTime)
+                .sum();
     }
 }
